6a6fbef7d5194db0664f1ec0e70aeb370d54df51
“chipo” <pnapoli33@gmail.com>
2022-04-10 17:22:19 -0300

    Initial commit

---
+++ src/test/java/com/demo/MainTests.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTests {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+}
